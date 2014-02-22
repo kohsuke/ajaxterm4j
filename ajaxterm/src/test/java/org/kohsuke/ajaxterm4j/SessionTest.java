2a747c837d505e667a8067c74bfa594bb764ddca
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-21 16:38:45 -0800

    if the default prompt length is too long, it'd cause "echo hello world" echo back to be split into two lines.
    set prompt to fix this

--- ajaxterm/src/test/java/org/kohsuke/ajaxterm4j/SessionTest.java
+++ ajaxterm/src/test/java/org/kohsuke/ajaxterm4j/SessionTest.java
@@ -15,10 +15,11 @@ public class SessionTest {
     public void basics() throws Exception {
         Session s = new Session(80,25,Session.getAjaxTerm(),"/bin/bash","-i");
         assertThat(s.isAlive(), is(true));
+        s.write("export PS1=:\n");
         s.write("echo hello world\n");
         Thread.sleep(5000);
         String dump = s.getTerminal().dumpLatin1();
-        assertThat(dump, containsString(" echo hello world"));
+        assertThat(dump, containsString(":echo hello world"));
         assertThat(dump, containsString("\nhello world"));
         System.out.println(dump);
         s.write("exit 3\n");
